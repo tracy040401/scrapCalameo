bef01d8247f97a6781332d4a39386df5a012cf1e
tracy040401 <andre.tracy@hotmail.fr>
2023-06-29 14:53:58 +0200

    other ideas for scrapping but not a lot of results

--- scrape.js
+++ scrape.js
@@ -4,73 +4,68 @@ async function openPage() {
     const browser = await puppeteer.launch({headless : false});
     const page = await browser.newPage();
 
-    // Navigate to the page
-    await page.goto('https://www.calameo.com/read/00614912823e04d409ee3');
+    /*
+    const ignoredPatterns = [/^[LESCUB]*$/, /^NC$/, /^[a-z]/, /^U\d+/, /Équipes/, /Secrétaire/, /Resp./, /FootLoisir/, /Membresclub/, /é/, /,/];
+    const ignoredLetters = ignoredPatterns.append([/[a-z]/,/[A-Z]/]);
+    const mailPattern = [/@/] ;
+    const clubPattern = [/[A-Z]/] ;
+    const jsonData = [];
+     */
 
-    // Click the "Accept All" button
-    await page.waitForSelector('.btn.btn-accept-all', { visible: true });
-    await page.click('.btn.btn-accept-all');
+    for (let i = 9 ; i <= 52 ; i ++){
+        const url = `https://p.calameoassets.com/221104151109-5586f6e74babea00e613666507e7c361/p${i}.svgz`;
+
+        await page.goto(url) ;
+        console.log(`PAGE ${i}`)
+        /*
 
 
-    // Click the button five times
-    for (let i = 0; i < 4; i++) {
-        await page.waitForSelector('.skin-tag.skin-button.action-right.skin-button-no-label.hide-on-mode-mini', { visible: true });
-        await page.click('.skin-tag.skin-button.action-right.skin-button-no-label.hide-on-mode-mini');
-        console.log('Page suivante : click', i)
-        await new Promise(resolve => setTimeout(resolve, 1 * 1000));
+        const elements = await page.$$('svg > text');
 
+        const pageData = {};
+        pageData.pageNumber = i;
+
+        let markedPhone1 = false ;
+        let markedMail1 = false ;
+        let markedMail
+        let markedClub = false ;
+        let club1 = '' ;
+        let club2 = '' ;
+
+        for (let j = 3; j <= elements.length; j++) {
+            const selector = `svg > text:nth-child(${j})`;
+            const textContent = await page.$eval(selector, element => element.textContent);
+            console.log(`${j}: ${textContent}`);
+
+            const key = j.toString();
+            const value = textContent.trim();
+
+            if (!markedPhone1){
+                if(!ignoredLetters.some((pattern) => pattern.test(value))) {
+                    pageData[key] = value;
+                    markedPhone = true ;
+                }
+                if(markedPhone1 && !markedMail1){
+                    if(mailPattern.some((pattern) => pattern.test(value))) {
+                        pageData[key] = value;
+                        markedMail = true ;
+                    }
+                }
+                if (markedMail)
+
+            if (!ignoredPatterns.some((pattern) => pattern.test(value))) {
+                pageData[key] = value;
+            }
+        }
+        jsonData.push(pageData) ;
+        console.log('============================================');
+        console.log(jsonData);
+
+         */
+
+        await new Promise(resolve => setTimeout(resolve, 2 * 60 * 1000));
     }
 
-    // Get all div elements with class "page-holder"
-    const pageHolderElements = await page.$$('.page-holder');
-
-    // Check if there are at least two div elements
-
-    const firstPageHolderElement = pageHolderElements[0];
-    const secondPageHolderElement = pageHolderElements[4];
-
-
-    // Get the img element within the first div element
-    const firstImgElement = await firstPageHolderElement.$('img.page');
-    const secondImgElement = await secondPageHolderElement.$('img.page');
-
-    // Get the src link within the first img element
-    const firstSrcLink = await firstImgElement.evaluate((element) => element.getAttribute('src'));
-    const secondSrcLink = await secondImgElement.evaluate((element) => element.getAttribute('src'));
-
-    console.log('first link :', firstSrcLink)
-    console.log('second link', secondSrcLink)
-
-    let link = firstSrcLink.startsWith('https:') ? firstSrcLink : `https:${firstSrcLink}`;
-
-    //console.log(link)
-    console.log('evaluation ok')
-
-    // Click the link
-    await page.goto(link);
-
-    await new Promise(resolve => setTimeout(resolve, 5 * 1000));
-
-    await page.goBack() ;
-
-    link = secondSrcLink.startsWith('https:') ? secondSrcLink : `https:${secondSrcLink}`;
-
-    // Click the link
-    await page.goto(link);
-
-    // Wait for the SVG element to be available
-    await page.waitForSelector('svg');
-
-    // Extract the text content of the element
-    const name_club = await page.$eval('svg > text:nth-child(17)', (element) => element.textContent);
-
-    console.log(name_club);
-
-
-    await new Promise(resolve => setTimeout(resolve, 2 * 60 * 1000));
-
-    // Take a screenshot of the page (optional)
-    await page.screenshot({ path: 'page.png' });
 
     // Close the browser
     await browser.close();
